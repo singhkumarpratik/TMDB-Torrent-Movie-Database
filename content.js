c225c031b154a744b6e1488774b4d471ab9c1032
Pratik <prxtiksingh@gmail.com>
2020-04-19 03:23:31 +0530

    fixed a bug where torrent links didn't appear as title year of the show/movie wasn't present (Mostly for old shows and movies)

--- content.js
+++ content.js
@@ -12,8 +12,18 @@ if(Object.values(imdbList).includes(locList)){
         var id=res[1].split("/")[0];
         item[i].innerHTML +='<span class="lister-item-year text-muted" style="margin-left:10px;margin-right:10px;">'+ '<a href=https://thepiratebay.org/search.php?q=' + id +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + id +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
     }
-}else{
+}
+else{
     locId=loc[1].split('/')[1];
     var item= document.getElementById('titleYear');
-    item.innerHTML +='<span class="lister-item-year text-muted">'+ '<a href=https://thepiratebay.org/search.php?q=' + locId +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + locId +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
+    if (item){
+        item.innerHTML +='<span class="lister-item-year text-muted">'+ '<a href=https://thepiratebay.org/search.php?q=' + locId +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + locId +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
+    }
+    else{
+        var item= document.getElementsByClassName("title_wrapper")[0];
+        var h1= item.getElementsByTagName('h1')[0];
+        var node = document.createElement("h1");
+        node.innerHTML='<span class="lister-item-year text-muted" style="margin-right:10px;">'+ '<a href=https://thepiratebay.org/search.php?q=' + locId +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + locId +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
+        h1.innerHTML+=node.innerHTML;
+    }
 }
